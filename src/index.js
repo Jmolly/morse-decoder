65bca382f88948ff16843a6ff82346ba74845ee6
roshkovskypetr <roshkovskypetr@gmail.com>
2019-09-22 23:41:42 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -38,8 +38,17 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let result = '';
+
+    for(let i = 0; i < expr.length; i += 10) {
+      let el = expr.slice(i, i+10);
+
+      (el === '**********') ? result += ' ':
+        result += MORSE_TABLE[el.replace(/11/g, '-').replace(/10/g, '.').replace(/0/g, '')];
+    }
+
+    return result;
+  }
 
 module.exports = {
     decode
